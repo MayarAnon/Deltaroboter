1667c6afa7680890247ee58ab76b9ec8c8de32af
Dennis Roth <dennisroth.4.2001@gmail.com>
2024-04-28 18:14:02 +0200

    Bash skript hinzugefügt

---
+++ Bash/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Festlegen des Basisverzeichnisses
+BASE_DIR="/home/pi/Deltarobot/Backend/WebServer"
+
+# Wechsle in das Verzeichnis oder breche bei Fehler ab
+cd "$BASE_DIR" || { echo "Fehler: Konnte nicht in das Verzeichnis $BASE_DIR wechseln"; exit 1; }
+
+# Starte die Services und Server
+nohup node server.js > server.log 2>&1 &
+nohup python ../StatePublisher/statepublisher.py > statepublisher.log 2>&1 &
+nohup python ../Homing/homing.py > homing.log 2>&1 &
+nohup python ../GripperControl/gripperControl.py > gripperControl.log 2>&1 &
+sudo nohup ../MotorControl/MotorControl > motorControl.log 2>&1 &
+nohup ../MotionPlaning/program > motionPlaning.log 2>&1 &
+
+echo "Alle Prozesse wurden gestartet."
