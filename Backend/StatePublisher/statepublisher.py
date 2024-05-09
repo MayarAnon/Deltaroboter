2a7c59d79ca62889906ef74f9bc525565fb679a5
Mayar Hanhon <mayar.hanhon@gmail.com>
2024-05-09 18:15:38 +0200

    add cleanup and logging

--- Backend/StatePublisher/statepublisher.py
+++ Backend/StatePublisher/statepublisher.py
@@ -1,6 +1,14 @@
-import paho.mqtt.client as mqtt
+import logging
 import json
-
+import paho.mqtt.client as mqtt
+from datetime import datetime
+# Konfiguration des Loggings
+logging.basicConfig(
+    filename='../../log/statepublisher.log',
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s:%(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 # MQTT-Konfiguration
 MQTT_BROKER = 'localhost'
 MQTT_PORT = 1883
@@ -27,7 +35,7 @@ robot_state = {
 
 # MQTT Callback für das Verbinden zum Broker
 def on_connect(client, userdata, flags, rc):
-    print(f"Connected with result code {rc}")
+    logging.info(f"Connected with result code {rc}")
     # Abonniere alle notwendigen Topics
     for topic in TOPICS:
         client.subscribe(topic)
@@ -61,15 +69,22 @@ def on_message(client, userdata, msg):
         # Veröffentliche den aktuellen Robotstate
         client.publish("robot/state", json.dumps(robot_state))
     except json.JSONDecodeError:
-        print("Error decoding JSON")
+        logging.error("Error decoding JSON")
     except ValueError as e:
-        print(e)
+        logging.error(e)
     except Exception as e:
-        print(f"Unexpected error: {str(e)}")
+        logging.error(f"Unexpected error: {str(e)}")
 # Initialisiere und starte den MQTT-Client
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
 client.connect(MQTT_BROKER, MQTT_PORT, 60)
-client.loop_forever()
+try:
+    client.loop_forever()
+except KeyboardInterrupt:
+    logging.info("Program interrupted by user")
+finally:
+    client.disconnect()
+    client.loop_stop()
+    logging.info("MQTT client disconnected and loop stopped.")
