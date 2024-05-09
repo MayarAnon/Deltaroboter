d708a7eb99e4f3479c7396c9ad9f15b7dd676070
Mayar Hanhon <mayar.hanhon@gmail.com>
2024-05-09 15:22:02 +0200

    comments added

--- Frontend/src/Components/IK.js
+++ Frontend/src/Components/IK.js
@@ -1,10 +1,10 @@
-// Definition der geometrischen Konstanten des Roboters
-const e = 173.21;     // Länge des Endeffektors
-const f = 346.41;    // Länge der Basis
-const re = 400.0; // Länge der Verbindungsglieder des Endeffektors
-const rf = 150.0; // Länge der Verbindungsglieder der Basis
+// Definition of geometric constants of the robot
+const e = 173.21;     // Length of the end effector
+const f = 346.41;    // Length of the base
+const re = 400.0; // Length of the end effector linkages
+const rf = 150.0; // Length of the base linkages
 
-// Definition trigonometrischer Konstanten
+// Definition of trigonometric constants
 const sqrt3 = Math.sqrt(3.0);
 const pi = Math.PI;
 const sin120 = sqrt3 / 2.0;
@@ -13,16 +13,16 @@ const tan60 = sqrt3;
 const sin30 = 0.5;
 const tan30 = 1 / sqrt3;
 
-// Funktion zur Berechnung des Winkels theta für die YZ-Ebene
+// Function to calculate the angle theta for the YZ plane
 function delta_calcAngleYZ(x0, y0, z0) {
-  const y1 = -0.5 * 0.57735 * f; // Berechnung basierend auf f/2 * tan(30)
-  y0 -= 0.5 * 0.57735 * e; // Verschiebung vom Zentrum zur Kante des Endeffektors
+  const y1 = -0.5 * 0.57735 * f; // Calculation based on f/2 * tan(30)
+  y0 -= 0.5 * 0.57735 * e; // Shift from center to edge of end effector
 
   const a = (x0 * x0 + y0 * y0 + z0 * z0 + rf * rf - re * re - y1 * y1) / (2 * z0);
   const b = (y1 - y0) / z0;
   const d = -(a + b * y1) * (a + b * y1) + rf * (b * b * rf + rf);
 
-  if (d < 0) return { status: -1, theta: null }; // Nicht existierender Punkt, wenn d < 0
+  if (d < 0) return { status: -1, theta: null }; // Non-existent point if d < 0
 
   const yj = (y1 - a * b - Math.sqrt(d)) / (b * b + 1);
   const zj = a + b * yj;
@@ -30,8 +30,7 @@ function delta_calcAngleYZ(x0, y0, z0) {
 
   return { status: 0, theta };
 }
-
-// Inverse Kinematik
+// Inverse Kinematics
 function delta_calcInverse(x0, y0, z0) {
   let theta1, theta2, theta3;
 
