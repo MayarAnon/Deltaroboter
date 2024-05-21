06c49671a06dc24f0177adf32b66e4b50b31d068
Mayar Hanhon <mayar.hanhon@gmail.com>
2024-05-21 22:05:59 +0200

    very important changes to the debugger

--- Frontend/src/Components/Debugger.jsx
+++ Frontend/src/Components/Debugger.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-
 import { Unity, useUnityContext } from "react-unity-webgl";
 import "../styles/debugger.css";
+
 const Debugger = () => {
   const { unityProvider } = useUnityContext({
     loaderUrl: "Build/FlappyBird.loader.js",
@@ -9,15 +9,17 @@ const Debugger = () => {
     frameworkUrl: "Build/build.framework.js",
     codeUrl: "Build/build.wasm",
   });
+
   const [cursors, setCursors] = useState([]);
-   // Direkte Überprüfung der Bildschirmbreite im Render-Zweig
-   
+  const [showUnity, setShowUnity] = useState(false);
+  const [showImage, setShowImage] = useState(false);
+
   useEffect(() => {
     const interval = setInterval(() => {
       setCursors((cursors) =>
         cursors.map((cursor) => ({
           ...cursor,
-          x: cursor.x + (Math.random() * 200 - 100), 
+          x: cursor.x + (Math.random() * 200 - 100),
           y: cursor.y + (Math.random() * 200 - 100),
         }))
       );
@@ -26,7 +28,14 @@ const Debugger = () => {
     return () => clearInterval(interval); // Bereinigung des Intervalls
   }, []);
 
-  const handleClick = (event) => {
+  const handleUnityClick = () => {
+    setShowUnity(true);
+    setShowImage(false);
+  };
+
+  const handleImageClick = (event) => {
+    setShowImage(true);
+    setShowUnity(false);
     // Fügt bei jedem Klick ein neues Bild hinzu
     for (let index = 0; index < 100; index++) {
       setCursors((prevCursors) => [
@@ -39,49 +48,80 @@ const Debugger = () => {
       ]);
     }
   };
+
   if (window.innerWidth <= 1024) {
     return null; // Zeigt nichts an, wenn die Bildschirmbreite 1024px oder weniger beträgt
-  }else{
-  return (
-    <>
-      {cursors.map(
-        (cursor, index) =>
-          cursor.visible && (
-            <img
-              key={index}
-              src="\image-removebg-preview.png" // Pfad zum Bild
-              className="cursor-image"
-              style={{
-                left: `${cursor.x}px`,
-                top: `${cursor.y}px`,
-                display: "block",
-                width: "17px",
-                height: "17px",
-                transform: 'rotate(15deg)'
-              }}
-              alt="Custom Cursor"
+  } else {
+    return (
+      <>
+        {showUnity && (
+          <div className="flex flex-col mt-5 items-center justify-center">
+            <Unity
+              unityProvider={unityProvider}
+              style={{ height: "50vw", maxHeight: "500px", width: "70vw" }}
             />
-          )
-      )}
-      <div className="flex flex-col items-center">
-        <div className="flex flex-col mt-5 items-center justify-center">
-          <Unity
-            unityProvider={unityProvider}
-            style={{ height: "50vw", maxHeight: "500px", width: "70vw" }}
-          />
-        </div>
-
-        <div onClick={handleClick}>
-          <div className="brick one"></div>
-          <div className="tooltip-mario-container">
-            <div className="box"></div>
-            <div className="mush"></div>
           </div>
-          <div className="brick two"></div>
-        </div>
-      </div>
-    </>
-  );}
-};
+        )}
 
+        {showImage && (
+          <img
+            src="/debugger.png" // Pfad zum Bild
+            className="debugger-image"
+            alt="Debugger"
+            style={{
+              position: "absolute",
+              left: "50%",
+              top: "70%",
+              width:"800px",
+              transform: "translate(-50%, -50%)",
+              zIndex: 10,
+            }}
+          />
+        )}
+
+        {cursors.map(
+          (cursor, index) =>
+            cursor.visible && (
+              <img
+                key={index}
+                src="/image-removebg-preview.png" // Pfad zum Bild
+                className="cursor-image"
+                style={{
+                  left: `${cursor.x}px`,
+                  top: `${cursor.y}px`,
+                  display: "block",
+                  width: "17px",
+                  height: "17px",
+                  transform: "rotate(15deg)",
+                }}
+                alt="Custom Cursor"
+              />
+            )
+        )}
+
+        <div className="flex flex-col items-center">
+          <div className="flex flex-row mt-5 items-center justify-center">
+            <div onClick={handleUnityClick} style={{ marginRight: "100px" }}>
+              <div className="brick one"></div>
+              <div className="tooltip-mario-container">
+                <div className="box"></div>
+                <div className="mush"></div>
+              </div>
+              <div className="brick two"></div>
+            </div>
+
+            <div onClick={handleImageClick} style={{ marginLeft: "100px" }}>
+              <div className="brick one"></div>
+              <div className="tooltip-mario-container">
+                <div className="box"></div>
+                <div className="mush"></div>
+              </div>
+              <div className="brick two"></div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+};
 export default Debugger;
